1b73340464cfcb49e9a4d15d63528bb062ebfd95
aui <g-keardkeawfa@rozetta.jp>
2020-02-20 19:44:33 +0900

    1

--- SampleApp/ConsoleApp/Program.cs
+++ SampleApp/ConsoleApp/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Runtime.InteropServices;
-using CommandLine;
+//using CommandLine;
 using ClassLibrary;
 using static System.Console;
 
@@ -10,22 +10,22 @@ namespace ConsoleApp
     {
         const string DEFAULT_MESSAGE = "Hello from .NET Framework!";
 
-        public class Options
-        {
-            [Option('m', "message", Required = false, HelpText = "Set output messages.")]
-            public string Message { get; set; }
-        }
+    //    public class Options
+    //    {
+    //        [Option('m', "message", Required = false, HelpText = "Set output messages.")]
+    //        public string Message { get; set; }
+    //    }
 
         static void Main(string[] args)
         {
             var whale = Utils.GetWhale();
-            var output = string.Empty;
-            var parser = new Parser(settings => { 
-                settings.IgnoreUnknownArguments = true; 
-            }).ParseArguments<Options>(args).WithParsed(options =>
-            {
-                output = String.IsNullOrEmpty(options.Message) ? $" {DEFAULT_MESSAGE}{whale}" : $" {options.Message}{whale}";
-            });
+            var output = $" {DEFAULT_MESSAGE}{whale}";
+    //        var parser = new Parser(settings => { 
+    //            settings.IgnoreUnknownArguments = true; 
+    //        }).ParseArguments<Options>(args).WithParsed(options =>
+    //        {
+    //            output = String.IsNullOrEmpty(options.Message) ? $" {DEFAULT_MESSAGE}{whale}" : $" {options.Message}{whale}";
+    //        });
             WriteLine(output);
             WriteLine("**Environment**");
             WriteLine($"Platform: .NET Framework");
